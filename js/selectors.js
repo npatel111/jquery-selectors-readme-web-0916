0986cbe4571946ede1ca9dc96f76ce165a37e0ec
Niti Nrw <niti.patel@flatironschool.com>
2016-11-05 11:30:58 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
+// ul:first-child
+// rn the first list item in the ul with the ID pic-list.
